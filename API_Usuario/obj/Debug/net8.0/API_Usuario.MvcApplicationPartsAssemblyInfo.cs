585664ba5684d4ba1dce6aa54614cfd4bb134066
Claudio Matheus <claudiomatheus055@gmail.com>
2024-11-06 23:15:14 -0300

    :sparkles: :feat: Iniciando API de cadastro de usuarios

--- API_Usuario/obj/Debug/net8.0/API_Usuario.MvcApplicationPartsAssemblyInfo.cs
+++ API_Usuario/obj/Debug/net8.0/API_Usuario.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("API_Usuario")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c727faa58dcdedb0af75f3acd7620cdffbdf9f")]
-[assembly: System.Reflection.AssemblyProductAttribute("API_Usuario")]
-[assembly: System.Reflection.AssemblyTitleAttribute("API_Usuario")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
